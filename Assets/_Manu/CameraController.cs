e154e26fcc411ab9c9046b690dd69d08d7937522
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-29 13:39:25 +0100

    Game folder (empty) and Manu folder with basic movement

---
+++ Assets/_Manu/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform player;
+	private float zOffset;
+    void Awake()
+    {
+        zOffset = transform.position.z - player.position.z;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zOffset);
+		
+    }
+}
---
+++ Assets/_Manu/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float movementSpeedInSpace = 10f;
+	public float turnSpeedInSpace = 10f;
+	public SpriteRenderer rocketFlame;
+	private Rigidbody2D m_Rigidbody;
+    
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+		rocketFlame.enabled = false;
+    }
+
+   
+    void FixedUpdate()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+		Vector2 movementDirection = -transform.right;
+		rocketFlame.enabled = movementInput.y > Mathf.Epsilon;
+
+		float forwardInput = Mathf.Max(0,movementInput.y);
+
+		m_Rigidbody.AddForce(forwardInput * movementSpeedInSpace * movementDirection, ForceMode2D.Force);
+		m_Rigidbody.AddTorque(-movementInput.x * turnSpeedInSpace);
+    }
+}
