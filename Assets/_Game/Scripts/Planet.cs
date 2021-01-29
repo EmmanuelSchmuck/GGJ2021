a3dc6cbdbe64e53b84503a046f2bcdc0180c033a
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-29 17:41:54 +0100

    Dog can land on planets and leave planets (press "up")

--- Assets/_Game/Scripts/Planet.cs
+++ Assets/_Game/Scripts/Planet.cs
@@ -6,17 +6,25 @@ public class Planet : MonoBehaviour
 {
 	public float gravityDistance = 10;
 	public float gravityMagnitude = 1f;
+
+	public float rotationSpeed;
 	[SerializeField] private CircleCollider2D m_CircleCollider;
+
+	public float Radius {get; private set;}
+
+	private Vector2 basePosition;
     // Start is called before the first frame update
     void Awake()
     {
-
+		basePosition = transform.position;
+		Radius = 2*m_CircleCollider.radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		float dt = Time.deltaTime;
+        transform.RotateAround(basePosition, Vector3.back, rotationSpeed * dt);
     }
 
 	private void OnDrawGizmos()
--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -13,16 +13,22 @@ public class PlayerController : MonoBehaviour
 	public float movementSpeedInSpace = 10f;
 	public float turnSpeedInSpace = 10f;
 	public float planetaryModeDistance;
+	public float movementSpeedOnPlanet = 10f;
+	public float altitudeOffset = 1f;
 	public SpriteRenderer rocketFlame;
 	private Rigidbody2D m_Rigidbody;
+	private Collider2D m_Collider;
 	private List<Planet> planets;
 	public SpriteRenderer body;
 
 	private Item currentItem;
+	private Planet currentPlanet;
+	private bool jumpBuffered;
 
 	private void Awake()
 	{
 		m_Rigidbody = GetComponent<Rigidbody2D>();
+		m_Collider = GetComponent<Collider2D>();
 		rocketFlame.enabled = false;
 		planets = GameObject.FindObjectsOfType<Planet>().ToList();
 	}
@@ -31,23 +37,67 @@ public class PlayerController : MonoBehaviour
 	private void FixedUpdate()
 	{
 		Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		Vector2 movementDirection = -transform.right;
-		rocketFlame.enabled = movementInput.y > Mathf.Epsilon;
 
-		float forwardInput = Mathf.Max(0, movementInput.y);
-
-		m_Rigidbody.AddForce(forwardInput * movementSpeedInSpace * movementDirection, ForceMode2D.Force);
-		m_Rigidbody.AddTorque(-movementInput.x * turnSpeedInSpace);
-
-		foreach (Planet planet in planets)
+		if (currentPlanet == null) // in space
 		{
-			Vector2 toPlanet = planet.transform.position - transform.position;
-			if (toPlanet.magnitude < planet.gravityDistance)
+			Vector2 movementDirection = -transform.right;
+			rocketFlame.enabled = movementInput.y > Mathf.Epsilon;
+
+			float forwardInput = Mathf.Max(0, movementInput.y);
+
+			m_Rigidbody.AddForce(forwardInput * movementSpeedInSpace * movementDirection, ForceMode2D.Force);
+			m_Rigidbody.AddTorque(-movementInput.x * turnSpeedInSpace);
+
+			foreach (Planet planet in planets)
 			{
-				Vector2 force = toPlanet.normalized * planet.gravityMagnitude / toPlanet.sqrMagnitude;
-				m_Rigidbody.AddForce(force, ForceMode2D.Force);
+				Vector2 toPlanet = planet.transform.position - transform.position;
+				if (toPlanet.magnitude < planet.gravityDistance)
+				{
+					Vector2 force = toPlanet.normalized * planet.gravityMagnitude / toPlanet.sqrMagnitude;
+					m_Rigidbody.AddForce(force, ForceMode2D.Force);
+				}
+				if (toPlanet.magnitude - planet.Radius < planetaryModeDistance && currentPlanet == null)
+				{
+					LandOnPlanet(planet);
+				}
 			}
 		}
+		else // on planet
+		{
+			Vector2 planetCoreToDog = transform.position - currentPlanet.transform.position;
+			float altitude = planetCoreToDog.magnitude - currentPlanet.Radius;
+			Vector2 movement = Vector3.Cross(Vector3.back,planetCoreToDog.normalized)*movementInput.x * movementSpeedOnPlanet;
+			transform.position += (Vector3)movement * Time.deltaTime;
+			planetCoreToDog = transform.position - currentPlanet.transform.position;
+			transform.position = currentPlanet.transform.position + (Vector3)planetCoreToDog.normalized * (currentPlanet.Radius+altitudeOffset);
+			transform.up = planetCoreToDog.normalized;
+			if(Mathf.Abs(movementInput.x) > Mathf.Epsilon)
+			{
+				body.transform.localScale = new Vector3(movementInput.x<0?1:-1,1,1);
+			}
+		}
+	}
+
+	private void LandOnPlanet(Planet planet)
+	{
+		currentPlanet = planet;
+		transform.SetParent(currentPlanet.transform);
+		m_Rigidbody.simulated = false;
+		m_Collider.isTrigger = true;
+		rocketFlame.enabled = false;
+	}
+
+	private void LeavePlanet(Planet planet)
+	{
+		Vector2 planetCoreToDog = transform.position - currentPlanet.transform.position;
+		transform.SetParent(null);
+		transform.position = currentPlanet.transform.position + (Vector3)planetCoreToDog.normalized * (currentPlanet.Radius+altitudeOffset+planetaryModeDistance+2f);
+		body.transform.localScale = Vector3.one;
+		m_Rigidbody.simulated = true;
+		m_Collider.isTrigger = false;
+		rocketFlame.enabled = false;
+		m_Rigidbody.AddForce(5*planetCoreToDog.normalized, ForceMode2D.Impulse);
+		currentPlanet = null;
 	}
 
 	private void Update()
@@ -69,6 +119,11 @@ public class PlayerController : MonoBehaviour
 				currentItem.OnRelease();
 				currentItem = null;
 			}
+		}
+
+		if(currentPlanet != null && Input.GetAxisRaw("Vertical")>Mathf.Epsilon)
+		{
+			LeavePlanet(currentPlanet);
 		}
 	}
 
