73e9c9e5b04ea188f88d46636a61971aff83870d
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-29 15:30:17 +0100

    planets with gravity

---
+++ Assets/_Manu/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	public float gravityDistance = 10;
+	public float gravityMagnitude = 1f;
+	[SerializeField] private CircleCollider2D m_CircleCollider;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnDrawGizmos()
+	{
+		float surfaceRadius = 2*m_CircleCollider.radius;
+
+		Gizmos.DrawWireSphere(transform.position, surfaceRadius);
+		Gizmos.DrawWireSphere(transform.position, gravityDistance);
+	}
+}
--- Assets/_Manu/PlayerController.cs
+++ Assets/_Manu/PlayerController.cs
@@ -1,30 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
+	public KeyCode actionKey = KeyCode.Space;
 	public float movementSpeedInSpace = 10f;
 	public float turnSpeedInSpace = 10f;
 	public SpriteRenderer rocketFlame;
 	private Rigidbody2D m_Rigidbody;
-    
-    void Awake()
-    {
-        m_Rigidbody = GetComponent<Rigidbody2D>();
-		rocketFlame.enabled = false;
-    }
+	private List<Planet> planets;
+	public SpriteRenderer body;
 
-   
-    void FixedUpdate()
-    {
-        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody2D>();
+		rocketFlame.enabled = false;
+		planets = GameObject.FindObjectsOfType<Planet>().ToList();
+	}
+
+
+	private void FixedUpdate()
+	{
+		Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		Vector2 movementDirection = -transform.right;
 		rocketFlame.enabled = movementInput.y > Mathf.Epsilon;
 
-		float forwardInput = Mathf.Max(0,movementInput.y);
+		float forwardInput = Mathf.Max(0, movementInput.y);
 
 		m_Rigidbody.AddForce(forwardInput * movementSpeedInSpace * movementDirection, ForceMode2D.Force);
 		m_Rigidbody.AddTorque(-movementInput.x * turnSpeedInSpace);
-    }
+
+		foreach (Planet planet in planets)
+		{
+			Vector2 toPlanet = planet.transform.position - transform.position;
+			if (toPlanet.magnitude < planet.gravityDistance)
+			{
+				Vector2 force = toPlanet.normalized * planet.gravityMagnitude / toPlanet.sqrMagnitude;
+				m_Rigidbody.AddForce(force, ForceMode2D.Force);
+			}
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(actionKey))
+		{
+			// do stuff
+		}
+	}
 }
