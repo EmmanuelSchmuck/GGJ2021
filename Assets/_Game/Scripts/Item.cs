76087b92080aaf4cee4fa7d085d2d9479263f1fc
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-29 15:37:57 +0100

    added Item class

---
+++ Assets/_Game/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// can be grabbed & released by dog
+[RequireComponent(typeof(Collider2D))]
+public class Item : MonoBehaviour
+{
+	
+}
