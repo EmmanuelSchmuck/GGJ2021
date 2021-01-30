f7f80bed1bd5e384862c7bf7d8038836de75d90a
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-30 12:36:46 +0100

    camera prefab with smooth follow

--- Assets/_Game/Scripts/CameraController.cs
+++ Assets/_Game/Scripts/CameraController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	public float minDistanceToFollow;
+	public float lerpSpeed;
 	public Transform player;
 	private float zOffset;
+
+	Vector3 targetPosition;
+	
     void Awake()
     {
         zOffset = transform.position.z - player.position.z;
+		targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, zOffset);
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zOffset);
+		float distance = Vector2.Distance(transform.position,player.transform.position);
+		if(distance>minDistanceToFollow)
+		{
+			targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, zOffset);
+		}
+		// targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, zOffset);
+		transform.position = Vector3.Lerp(transform.position, targetPosition, (distance/10) * Time.deltaTime * lerpSpeed);
+        
 		
     }
 }
