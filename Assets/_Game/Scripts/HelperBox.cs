acb0eaa048a845a9887811e7631163046af8a688
Emmanuel Schmuck <emmanuel.schmuck@gmail.com>
2021-01-30 11:28:15 +0100

    HelperBox API

---
+++ Assets/_Game/Scripts/HelperBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperBox : MonoBehaviour
+{
+    public static void DisplayText(HelperBoxText text)
+	{
+		Debug.Log($"Displaying helper box text: {text}");
+	}
+}
+
+public enum HelperBoxText
+{
+	None,
+	PlanetMovement,
+	CatchItem,
+	GiveItem,
+	JumpToSpace,
+	SpaceMovement
+}
